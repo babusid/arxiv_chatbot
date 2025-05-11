64d8426de7db32560a0145e72d681a74e2d827f7
Sidharth N. Babu <sidharth.n.babu@gmail.com>
2025-05-10 19:23:51 -0500

    refactor

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ Main module for pdf -> markdown conversion API
 
 from fastapi import FastAPI, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
-import enum
-from uuid import uuid4
-import pymupdf4llm
-import tempfile, os
-from urllib.request import urlretrieve
+import parser
 
 origins = [
     "http://localhost",
@@ -29,36 +25,6 @@ app.add_middleware(
 )
 
 
-class JobStatus(enum.Enum):
-    """
-    Enum for job status
-    """
-
-    QUEUED = "queued"
-    COMPLETED = "completed"
-    FAILED = "failed"
-
-
-# stores job status - eventually this should be a database table
-job_status_dict: dict[str, dict] = {}
-
-
-def convert_pdf_to_markdown(job_id: str, pdf_path: str) -> str:
-    """
-    Converts a PDF file to markdown using pymupdf4llm
-    """
-    fd, path = tempfile.mkstemp(suffix=".pdf")
-    os.close(fd)
-    try:
-        urlretrieve(pdf_path, path)
-        markdown = pymupdf4llm.to_markdown(path, embed_images=True)
-    finally:
-        # 4. No footprint left behind
-        os.remove(path)
-    job_status_dict[job_id]["status"] = JobStatus.COMPLETED
-    job_status_dict[job_id]["markdown"] = markdown
-
-
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
@@ -70,13 +36,7 @@ def init_conversion(pdf_path: str, bg: BackgroundTasks):
     Starts the conversion process for the given PDF file,
     returns a job ID. Conversion should start in the background via a worker.
     """
-    print(pdf_path)
-    print(job_status_dict)
-
-    job_id = uuid4().hex
-    job_status_dict[job_id] = {"status": JobStatus.QUEUED}
-    bg.add_task(convert_pdf_to_markdown, job_id, pdf_path)
-    return {"job_id": job_id}
+    return parser.add_parse_job(pdf_path, bg)
 
 
 @app.get("/status/{job_id}")
@@ -84,9 +44,7 @@ def get_status(job_id: str):
     """
     Returns the status of the conversion job.
     """
-    if job_id not in job_status_dict:
-        return {"error": "Job ID not found"}
-    return job_status_dict[job_id]["status"]
+    return parser.get_parse_job_status(job_id)
 
 
 @app.get("/result/{job_id}")
@@ -94,8 +52,4 @@ def get_result(job_id: str):
     """
     Returns the result of the conversion job.
     """
-    if job_id not in job_status_dict:
-        return {"error": "Job ID not found"}
-    if job_status_dict[job_id]["status"] != JobStatus.COMPLETED:
-        return {"error": "Job is not completed yet"}
-    return job_status_dict[job_id]["markdown"]
+    return parser.get_parse_job_result(job_id)
---
+++ parser.py
@@ -0,0 +1,64 @@
+from uuid import uuid4
+import pymupdf4llm
+import tempfile, os
+from urllib.request import urlretrieve
+import enum
+from fastapi import BackgroundTasks
+
+
+class JobStatus(enum.Enum):
+    """
+    Enum for job status
+    """
+
+    QUEUED = "queued"
+    COMPLETED = "completed"
+    FAILED = "failed"
+
+
+# stores job status - eventually this should be a database table
+parse_jobs: dict[str, dict] = {}
+
+
+def add_parse_job(pdf_path: str, bg: BackgroundTasks) -> dict:
+    '''
+    Creates and adds a new parse job to the queue, returns a job ID.
+    '''
+    job_id = uuid4().hex
+    parse_jobs[job_id] = {"status": JobStatus.QUEUED}
+    bg.add_task(convert_pdf_to_markdown, job_id, pdf_path)
+    return {"job_id": job_id}
+
+def get_parse_job_status(job_id: str) -> dict:
+    """
+    Returns the status of the parse job
+    """
+    if job_id not in parse_jobs:
+        return {"error": "Job ID not found"}
+    return parse_jobs[job_id]["status"]
+
+def get_parse_job_result(job_id: str) -> dict:
+    """
+    Returns the result of the parse job
+    """
+    if job_id not in parse_jobs:
+        return {"error": "Job ID not found"}
+    if parse_jobs[job_id]["status"] != JobStatus.COMPLETED:
+        return {"error": "Job is not completed yet"}
+    return parse_jobs[job_id]["markdown"]
+
+
+def convert_pdf_to_markdown(job_id: str, pdf_path: str) -> str:
+    """
+    Converts a PDF file to markdown using pymupdf4llm
+    """
+    fd, path = tempfile.mkstemp(suffix=".pdf")
+    os.close(fd)
+    try:
+        urlretrieve(pdf_path, path)
+        markdown = pymupdf4llm.to_markdown(path, embed_images=True)
+    finally:
+        # 4. No footprint left behind
+        os.remove(path)
+    parse_jobs[job_id]["status"] = JobStatus.COMPLETED
+    parse_jobs[job_id]["markdown"] = markdown
